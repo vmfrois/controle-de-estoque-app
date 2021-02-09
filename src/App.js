b46981ba81dae7f19a9aa0398a434f16b620ac00
Vinicius Frois <viniciusfroism@gmail.com>
2021-02-09 15:51:04 -0300

    Trabalhando com estado e comportamento nos componentes/ revisao de react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-        hello world!
-    </div>
-  );
+class App extends React.Component{
+
+    state = {
+      numero1: null,
+      numero2: null,
+      resultado: null
+    }
+
+    somar = () => {
+      const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2);
+      this.setState({resultado: resultado});
+    }
+
+    render(){
+      return(
+        <div>
+          <label>Primeiro numero: </label>
+          <input type="text" value={this.state.numero1} 
+                  onChange={ (e) => this.setState({numero1: e.target.value}) } />  
+          <br />
+          <label>Segundo numero: </label>
+          <input type="text" value={this.state.numero2}
+                  onChange={(e) => this.setState({numero2: e.target.value})}/>
+          <br />
+          <button onClick={this.somar}>Somar</button>
+          <br />
+          A soma dos dois numeros é: {this.state.resultado}
+        </div>
+      )
+    }
+
+
 }
 
 export default App;
